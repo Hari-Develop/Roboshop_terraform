78680faf46263b225dfc1861484c3634c6b18cff
HarinadhaReddy Yadala <harinadhareddyyadala@HarinadhaReddys-MacBook-Air.local>
2023-05-08 11:23:57 +0530

    file changes

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "terraform-a72"
+        bucket = "terraform-72"
         key    = "roboshop/dev/terraform.tfstate"
         region = "us-east-1"
     }
