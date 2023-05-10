5865938bae55cb2cbc1aef5802ec3c62e3d0f17a
Hari-Develop <120960644+Hari-Develop@users.noreply.github.com>
2023-05-10 16:26:59 +0530

    added

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-variable "app_server" {
-default = {
-    frontend = {
-        name = "frontend"
-        instance_type = "t3.micro"
-    }
-    catalogue = {
-        name = "catalogue"
-        instance_type = "t3.micro"
-    }
-    cart = {
-        name = "cart"
-        instance_type = "t3.micro"
-    }
-    user = {
-        name = "user"
-        instance_type = "t3.micro"
-    }
-    payment = {
-        name = "payment"
-        instance_type = "t3.micro"
-    }
-    shipping = {
-        name = "shipping"
-        instance_type = "t3.micro"
-    }
-}
-    }
 
 resource "aws_instance" "instance" {
     for_each = var.app_server
