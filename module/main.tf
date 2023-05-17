d7c6bba3a9c961a25f57b51f7d0661acf27300ce
harinadharedddy <yadalaharinadhareddy01@gmail.com>
2023-05-17 07:22:13 +0530

    file changes

--- module/main.tf
+++ module/main.tf
@@ -17,7 +17,7 @@ connection {
     type     = "ssh"
     user     = "centos"
     password = "DevOps321"
-    host     = "[aws_instance.instance.private_ip]"
+    host = aws_instance.instance.private_ip
   }
 
 provisioner "remote-exec" {
